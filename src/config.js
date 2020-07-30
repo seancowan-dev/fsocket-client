ddf5836d2d71e1f93771798b1be068795d39a05d
Sean <sean.cowan1@gmail.com>
2020-07-30 14:49:56 -0400

    added unncessary boilerplate

--- src/config.js
+++ src/config.js
@@ -2,7 +2,4 @@ const Config = {
     SOCKET_URL: "https://damp-falls-21610.herokuapp.com",
 }
 
-// http://localhost:8000 - for dev
-// https://damp-falls-21610.herokuapp.com - for live
-
 export default Config
